6411e6d47c2770e180277048d7b0a55ac073bf13
Jevon Ivander Juandy <reihimawari27@gmail.com>
2023-10-14 14:40:42 +0700

    Create Database

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -11,11 +11,12 @@
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css'])
  </head>
+
  <body>
     <!-- Responsive navbar-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#!">Start Bootstrap</a>
+            <a class="navbar-brand" href="{{ route('landing') }}">UTS-2021130021</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
@@ -27,20 +28,22 @@
             </div>
         </div>
     </nav>
+
     <!-- Page header with logo and tagline-->
     <header class="py-5 bg-light border-bottom mb-4">
         <div class="container">
             <div class="text-center my-5">
-                <h1 class="fw-bolder">Welcome to Blog Home!</h1>
-                <p class="lead mb-0">A Bootstrap 5 starter layout for your next blog homepage</p>
+                <h1 class="fw-bolder">UTS PBWL - Transaction</h1>
+                <p class="lead mb-0">Ujian Tengah Semester Mata Kuliah Pemrograman Berbasis Web Lanjut</p>
             </div>
         </div>
     </header>
     <!-- Page content-->
         @yield('content')
-         <!-- Footer-->
-         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
-        </footer>
-    </body>
+
+    <!-- Footer-->
+    <footer class="py-5 bg-dark">
+        <div class="container"><p class="m-0 text-center text-white">Copyright ©️{{date('Y')}} <a href="/">UTS-2021130021</a></p></div>
+    </footer>
+</body>
 </html>
