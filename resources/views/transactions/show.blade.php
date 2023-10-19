141f797568ae5c07edd72f00e0162aba14562142
Jevon Ivander Juandy <reihimawari27@gmail.com>
2023-10-19 18:12:04 +0700

    Adjust show

--- resources/views/transactions/show.blade.php
+++ resources/views/transactions/show.blade.php
@@ -3,13 +3,14 @@
 @section('title', $transaction->id)
 
 @section('content')
-    <article class="blog-post my-4">
-        <h1 class="display-5 fw-bold">{{ $transaction->id }}</h1>
-        <p class="display-6">Amount : {{ $transaction->amount }}</p>
-        <p class="display-6">Type : {{ $transaction->type }}</p>
-        <p class="display-6">Category : {{ $transaction->category }}</p>
-        <p class="display-6">Notes : {{ $transaction->notes }}</p>
-        <p class="blog-post-meta">{{ $transaction->updated_at }}</p>
-        <p>{{ $book->body }}</p>
-    </article>
+    <div class="d-flex justify-content-center align-items-center">
+        <section class="blog-post">
+            <h1 class="display-5 fw-bold">Id : {{ $transaction->id }}</h1>
+            <p class="display-6">Amount : {{ $transaction->amount }}</p>
+            <p class="display-6">Type : {{ ucfirst($transaction->type) }}</p>
+            <p class="display-6">Category : {{ ucfirst($transaction->category) }}</p>
+            <p class="display-6">Notes : {{ $transaction->notes }}</p>
+            <p class="blog-post-meta">Updated : {{ $transaction->updated_at }}</p>
+        </section>
+    </div>
 @endsection
